70737be7eac9befc1e497c4bb9129b5223482606
Florian Thiel <flth@flth.de>
2025-01-23 01:25:50 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,56 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		tags = { "europe", "train", "deutsche bahn", "ice", "high speed" },
+
+        params = {
+			{
+				key = "icelfake",
+				name = _("Fake"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_icel_desc"),
+				defaultIndex = 0,
+			},
+		},
+
+		dependencies = { },
+
+        requiredMods = { },
+
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+        url = { "" },
+        visible = true,
+	},
+
+	runFn = function (settings, modParams)
+
+	    local hidden = {
+
+		}
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/icel_([^/]*.lua)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["icelfake"] == 0 then
+				addFileFilter("multipleUnit", modelFilter)
+			end
+		else
+			addFileFilter("multipleUnit", modelFilter)
+		end
+	end
+	}
+end
---
+++ res/config/multiple_unit/icel.lua
@@ -0,0 +1,10 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/waggon/ICEL/stw.mdl", forward = true },
+	},
+	name = _("ICEL_name"),
+	desc = _("ICEL_desc"),
+    groupFileName = "vehicle/train/ICEL/menu_ICEL.mdl",
+}
+end
