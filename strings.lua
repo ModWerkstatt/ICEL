56ef9271eeec2d2796b1d2e3888858bfaac584f0
Florian Thiel <flth@flth.de>
2025-02-09 22:58:37 +0100

    Strings aktualisiert

--- strings.lua
+++ strings.lua
@@ -2,7 +2,7 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "ICE L ofDeutschen Bahn",
+		["mod_name"] = "ICE L (incl. Class 105) of Deutsche Bahn",
 		["mod_desc"] = "The ICE L is the latest addition to the ICE series and differs fundamentally from the previous ICEs. It is a locomotive-hauled train instead of a multiple unit. Originally it was to run with a class 105 Talgo locomotive, but a Vectro dual-mode is now planned for the time being. The train will primarily replace the last remaining IC1 trains and will also run to Westerland/Sylt.\nThis mod also includes the fake version of all vehicles. These can be activated with the ingame mod manager if required.",
 
         ["Fake"] = "Activate fake versions?",
@@ -35,13 +35,13 @@ return {
 		["ICEL_Apzf17_name"] = "Coach 17, control car Apzf 896.1",
 		["ICEL_laufwerk_name"] = "Single-axis drive portal",
 
-        ["br105_name"] = "BR 105",
+        ["br105_name"] = "Class 105",
         ["br105_desc"] = "The locomotive for the ICE L is also to be supplied by Talgo and bears the series designation 105. The locomotives are currently in acceptance runs and are scheduled to be used together with the ICE L from mid-2025.",
 
 	},
 	de = {
 
-		["mod_name"] = "ICE L der Deutschen Bahn",
+		["mod_name"] = "ICE L (inkl. BR 105) der Deutschen Bahn",
 		["mod_desc"] = "Der ICE L ist der neueste Teil der ICE-Baureihe und unterscheidet sich grundlegend von den bisherigen ICE. Er ist ein Lok gezogener Zug anstatt eines Triebzugs. Ursprünglich sollte er mit einer Talgo-Lokomotive der Baureihe 105 verkehren, mittlerweile ist eine Vectron-Dualmode vorerst geplant. Ersetzen soll der Zug vor allem die letzten verbliebenen IC1-Züge und u.a. auch nach Westerland/Sylt fahren.\nDieser Mod beinhaltet ausserdem die Fake-Variante von allen Fahrzeugen. Diese können mit dem Ingame-Mod-Manager bei Bedarf aktiviert werden.",
 
         ["Fake"] = "Fake-Versionen aktivieren?",
