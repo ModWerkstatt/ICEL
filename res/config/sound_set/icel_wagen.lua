b9f258e4e4f52535de5d9b5e36110032ec64e74a
Florian Thiel <flth@flth.de>
2025-02-18 20:28:38 +0100

    Fix Soundset

--- res/config/sound_set/icel_wagen.lua
+++ res/config/sound_set/icel_wagen.lua
@@ -2,6 +2,7 @@ local soundeffectsutil = require "soundeffectsutil"
 
 function data()
 return {
+    tracks = { },
 	events = {
 		openDoors = { names = { "vehicle/ICEL/dooropen.wav" }, refDist = 10.0 },
 		closeDoors = { names = { "vehicle/ICEL/doorclose.wav" }, refDist = 10.0 }
@@ -9,6 +10,7 @@ return {
 
 	updateFn = function (input)
 		return {
+            tracks = { },
 			events = {
 				openDoors = { gain = 1.00, pitch = 1.0 },
 				closeDoors = { gain = 1.00, pitch = 1.0 }
